45e1042ccac0444ceb2d84a0c52cb3010d167987
RainyDay7 <1764682836@qq.com>
2019-10-21 22:54:08 +0800

    three numbers add up to 0

---
+++ 15threeSum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+vector<vector<int>> threeSum(vector<int>& nums) {
+        sort( nums.begin(), nums.end());
+        vector<vector<int>> res;
+        
+        if( nums.size() < 3 || nums[0] > 0 || nums[ nums.size() - 1] < 0)
+            return res;
+        
+        for( int i = 0; i < nums.size(); i++){
+            if( nums[i] > 0)
+                break;
+            
+            if( i > 0 && nums[i] == nums[i-1])
+                continue;
+            
+            int l = i + 1, r = nums.size() - 1;
+            while( l < r){
+                if( nums[i] + nums[l] + nums[r] == 0){
+                    vector<int> temp = { nums[i] , nums[l] , nums[r]};
+                    res.push_back( temp);
+                    
+                    //ШЅжи
+                    while( l<r && nums[l]==nums[l+1])l++;
+                    while( l<r && nums[r]==nums[r-1])r--;
+                    
+                    l++;
+                    r--;
+                }
+                else if( nums[i] + nums[l] + nums[r] > 0)
+                    r--;
+                else if( nums[i] + nums[l] + nums[r] < 0)
+                    l++;
+            }
+        }
+        
+        return res;
+    }
+
+
+};
